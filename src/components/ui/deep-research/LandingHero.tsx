90c7938b15f0d0585a4cfba5e855d551f826fae6
Julio Cervantes <imjcervantes@gmail.com>
2025-04-16 16:43:40 -0700

    replace typewriter-effect

--- src/components/ui/deep-research/LandingHero.tsx
+++ src/components/ui/deep-research/LandingHero.tsx
@@ -2,33 +2,33 @@
 
 import { useAuth } from "@clerk/nextjs"
 import Link from "next/link";
-import TypewriterComponent from "typewriter-effect"
+import { Typewriter } from 'react-simple-typewriter';
 import { Button } from "../button";
 
 export const LandingHero = () => {
     const { isSignedIn } = useAuth();
 
     return (
-        <div className="text-white font-bold py-36 text-center space-y-5"> 
+        <div className="text-white font-bold py-36 text-center space-y-5">
             <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
                 <h1>
                     La mejor herramienta para
                 </h1>
                 <div className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-violet-500">
-                    <TypewriterComponent 
-                        options={{
-                            strings: ["investigación.", "educación.", "creación de contenido.", "blogs.", "y más."],
-                            autoStart: true,
-                            loop: true,
-                            delay: 75,
-                            deleteSpeed: 50,
-                        }}
+                    <Typewriter
+                        words={["investigación.", "educación.", "creación de contenido.", "blogs.", "y más."]}
+                        loop={true}
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
                     />
                 </div>
-            </div> 
+            </div>
             <div className="text-sm md:text-xl font-light text-zinc-400">
-                Genera apuntes de investigación completos y personalizados en segundos. 
-                <br /> 
+                Genera apuntes de investigación completos y personalizados en segundos.
+                <br />
                 Potenciado por IA y diseñado para estudiantes y profesionales.
             </div>
 
